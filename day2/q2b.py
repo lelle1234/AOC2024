605ad11579b8a24381bddff897b77eae29562d36
Lennart Jonsson <lennart.jonsson@umu.se>
2024-12-02 18:47:22 +0100

    Simplifications

--- day2/q2b.py
+++ day2/q2b.py
@@ -9,12 +9,10 @@ inbounds = lambda r: np.all(abs(np.diff(r)) >= 1) & np.all(abs(np.diff(r)) <= 3)
 
 def check_line(inp):
     arr = np.fromstring(inp, dtype=int, sep=' ')
-    if monotonic(arr) and inbounds(arr):
-        return True
-
-    for parr in combinations(arr, len(arr)-1):
-        if monotonic(parr) and inbounds(parr):
-            return True
+    for n in range(2):
+        for parr in combinations(arr, len(arr)-n):
+            if monotonic(parr) and inbounds(parr):
+                return True
     return False
 
 num = 0
